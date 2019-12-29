a47c465179f8e8e51d3f86365f946127e533f790
Eli Saado <eli5saado@gmail.com>
2019-12-29 21:00:54 +0100

    Add debug logger

--- app.js
+++ app.js
@@ -1,8 +1,12 @@
 'use strict';
 
 const WebSocket = require('ws');
+const debug = require('debug')('felt-server');
 
-const wss = new WebSocket.Server({ port: 8080 });
 const connectionHandler = require('./handlers/connection.js');
 
+debug('Initializing server...')
+const wss = new WebSocket.Server({ port: 8080 });
 wss.on('connection', connectionHandler);
+
+debug('Listening on 0.0.0.0:%o', wss.address());
