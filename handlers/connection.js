2fb4408376f6d52762e6c5afbf75366213cb4a4f
Eli Saado <eli5saado@gmail.com>
2020-01-06 22:37:39 +0100

    Add connection handler some logic

--- handlers/connection.js
+++ handlers/connection.js
@@ -1,9 +1,23 @@
 'use strict';
 
+const rooms = {};
+
+// This function is run for every new connection
 function connection(ws) {
+    console.log(ws);
+
     ws.on('message', (message) => {
-        console.log('received: %s', message);
-        ws.send(message);
+        // check for undefined or null message
+        if (!message || !message.length) return;
+
+        const parts = message.split(" ")
+            .map(part => part.toLowerCase());
+
+        switch (parts[0]) {
+            case "":
+            default:
+
+        }
     });
 }
 
