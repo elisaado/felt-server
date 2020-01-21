45e80894c2e1e697c670e7f5aadff6da5381281d
Eli Saado <eli5saado@gmail.com>
2020-01-21 22:08:33 +0100

    ESlint and preparing for handlers

--- router.js
+++ router.js
@@ -1,55 +1,44 @@
-'use strict';
-
-const rooms = {};
+const store = {
+  rooms: {},
+};
 
 // This function is run for every new connection
 function connection(ws) {
-    ws.on('message', (message) => {
-        // check for undefined or null message
-        if (!message || !message.length) return;
+  ws.on('message', (message) => {
+    // check for undefined or null message
+    if (!message || !message.length) return;
 
-        const parts = message.split(" ")
-            .map(part => part.toLowerCase());
+    const parts = message.split(' ')
+      .map((part) => part.toLowerCase());
 
-        switch (parts[0]) {
-            case "ident": {
-                console.log(ws.data);
+    const args = parts.slice(1);
 
-                if (!ws.data) { // Check if ws.data exists
+    switch (parts[0]) {
+      case 'ident': {
+        break;
+      } case 'init': {
+        break;
+      } case 'kill': {
+        break;
+      } case 'join': {
+        break;
+      } case 'quit': {
+        break;
+      } case 'left': {
+        break;
+      } case 'right': {
+        break;
+      } case 'up': {
+        break;
+      } case 'down': {
+        break;
+      }
 
-                }
-                break;
-            } case "init": {
-
-                break;
-            } case "kill": {
-
-                break;
-            } case "join": {
-
-                break;
-            } case "quit": {
-
-                break;
-            } case "left": {
-
-                break;
-            } case "right": {
-
-                break;
-            } case "up": {
-
-                break;
-            } case "down": {
-
-                break;
-            }
-
-            default: {
-                ws.send("Unknown command");
-            }
-        }
-    });
+      default: {
+        ws.send('Unknown command');
+      }
+    }
+  });
 }
 
 module.exports = connection;
