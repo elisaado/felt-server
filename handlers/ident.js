1d0108d8061ce8925364d665ad29d8465b18f4e9
Eli Saado <eli5saado@gmail.com>
2020-01-23 10:44:37 +0100

    Fix crash when no args provided

--- handlers/ident.js
+++ handlers/ident.js
@@ -1,8 +1,9 @@
 module.exports = ({ ws, args, store }) => {
+  if (args.length === 0) return ws.send('No arguments provided');
   const component = args[0].toUpperCase();
 
   if (component !== 'CTL' && component !== 'SRV' && component !== 'PRN') return ws.send('Invalid client');
 
   ws.data.component = component;
-  ws.send('OK');
+  return ws.send('OK');
 };
