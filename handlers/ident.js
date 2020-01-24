a4e0b3bfeafbdb15502a71a5a89a0c3bd7f8b593
Eli Saado <eli5saado@gmail.com>
2020-01-24 15:26:29 +0100

    Remove SRV from components

--- handlers/ident.js
+++ handlers/ident.js
@@ -3,7 +3,7 @@ module.exports = ({ ws, args, store }) => {
   if (args.length === 0) return ws.send('No arguments provided');
   const component = args[0].toUpperCase();
 
-  if (component !== 'CTL' && component !== 'SRV' && component !== 'PRN') return ws.send('Invalid client');
+  if (component !== 'CTL' && component !== 'PRN') return ws.send('Invalid client');
 
   ws.data.component = component;
   return ws.send('OK');
