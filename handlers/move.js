6cf44798386df48dfbb5ef4ce5bf6d0810ec7887
Eli Saado <eli5saado@gmail.com>
2020-02-22 21:47:48 +0100

    Make move handler production-ready

--- handlers/move.js
+++ handlers/move.js
@@ -3,6 +3,15 @@ module.exports = ({ ws, args, store }) => {
   if (ws.data.client !== 'CTL') return ws.send('Invalid ident');
   if (!ws.data.roomNumber) return ws.send('Not in a room');
 
+  const movements = [
+    'left',
+    'right',
+    'up',
+    'down',
+  ];
+
+  if (!movements.includes(args[0])) return ws.send('Invalid movement');
+
   store.rooms[ws.data.roomNumber].presentation.send(args[0]);
 
   return ws.send('OK');
