90bd906d30234f8a29c504a5a1195f8ca6dad5ed
Eli Saado <eli5saado@gmail.com>
2020-02-22 21:48:04 +0100

    Add maxpayload option

--- app.js
+++ app.js
@@ -4,7 +4,7 @@ const debug = require('debug')('felt-server');
 const router = require('./router.js');
 
 debug('Initializing server...');
-const wss = new WebSocket.Server({ port: 8080 });
+const wss = new WebSocket.Server({ port: 8080, maxPayload: 1024 });
 wss.on('connection', router);
 
 debug('Listening on 0.0.0.0:%o', wss.address().port);
